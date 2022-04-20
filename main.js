c14778c59a7cb55c34a86ea0c783017c467d64a8
tszkiu <tszkiu@gmail.com>
2022-04-20 16:44:06 +0200

    nav bar

--- main.js
+++ main.js
@@ -1,17 +1,42 @@
 console.log(avocadoData[0].title);
 
 let body = document.querySelector("body");
+body.classList.add("container-fluid");
+
+let nav = document.querySelector("nav");
+nav.style.display = "flex";
+nav.style.alignItems = "center";
+// nav.style.justifyContent = "center";
+
 let mainContainer = document.createElement("main");
+mainContainer.classList.add("row");
+mainContainer.classList.add("justify-content-sm-center");
 body.appendChild(mainContainer);
 
-for (i = 0; i < avocadoData.length; i++) {
-  let divCard = document.createElement("div");
+for (i = 1; i < avocadoData.length; i++) {
+  let sectionCard = document.createElement("section");
+  sectionCard.classList.add("col-md-4");
+  if (i % 2 === 0) {
+    sectionCard.classList.add("bg-warning");
+  }
+  sectionCard.style.display = "flex";
+  sectionCard.style.flexDirection = "column";
+  sectionCard.style.justifyContent = "flex-end";
+  sectionCard.style.padding = "10px";
+
+  let headerContainer = document.createElement("div");
   let h5 = document.createElement("h5");
   h5.innerHTML = avocadoData[i].title;
+
+  let imgContainer = document.createElement("div");
   let img = document.createElement("img");
   img.setAttribute("src", avocadoData[i].image);
+  img.classList.add("img-fluid");
+  img.style.alignSelf = "flex-end";
 
-  divCard.appendChild(h5);
-  divCard.appendChild(img);
-  mainContainer.appendChild(divCard);
+  headerContainer.appendChild(h5);
+  sectionCard.appendChild(headerContainer);
+  imgContainer.appendChild(img);
+  sectionCard.appendChild(imgContainer);
+  mainContainer.appendChild(sectionCard);
 }
