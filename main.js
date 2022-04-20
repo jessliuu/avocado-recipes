deb496b19af5fb73bbca71dafa3c3d26230b3e19
tszkiu <tszkiu@gmail.com>
2022-04-20 16:44:52 +0200

    styling

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-console.log(avocadoData[0].title);
+// console.log(avocadoData);
+// console.log(avocadoData[0].missedIngredients);
 
 let body = document.querySelector("body");
 body.classList.add("container-fluid");
+body.style.fontFamily = "Avenir Book";
 
 let nav = document.querySelector("nav");
 nav.style.display = "flex";
@@ -16,9 +18,11 @@ body.appendChild(mainContainer);
 for (i = 1; i < avocadoData.length; i++) {
   let sectionCard = document.createElement("section");
   sectionCard.classList.add("col-md-4");
-  if (i % 2 === 0) {
-    sectionCard.classList.add("bg-warning");
-  }
+  sectionCard.style.border = "1px solid #d387ab";
+  sectionCard.style.borderTop = "0px";
+  // if (i % 2 === 0) {
+  //   sectionCard.style.backgroundColor = "#eec0c6";
+  // }
   sectionCard.style.display = "flex";
   sectionCard.style.flexDirection = "column";
   sectionCard.style.justifyContent = "flex-end";
@@ -27,6 +31,7 @@ for (i = 1; i < avocadoData.length; i++) {
   let headerContainer = document.createElement("div");
   let h5 = document.createElement("h5");
   h5.innerHTML = avocadoData[i].title;
+  h5.style.color = "#d387ab";
 
   let imgContainer = document.createElement("div");
   let img = document.createElement("img");
@@ -39,4 +44,16 @@ for (i = 1; i < avocadoData.length; i++) {
   imgContainer.appendChild(img);
   sectionCard.appendChild(imgContainer);
   mainContainer.appendChild(sectionCard);
+
+  let ingredientsContainer = document.createElement("div");
+  let ingredients = document.createElement("p");
+
+  for (j = 0; j < avocadoData[i].missedIngredients.length; j++) {
+    let missedIngredients = [];
+    missedIngredients.push(avocadoData[i].missedIngredients[j].name);
+    ingredients.innerHTML = missedIngredients;
+    ingredientsContainer.appendChild(ingredients);
+  }
+
+  sectionCard.appendChild(ingredientsContainer);
 }
