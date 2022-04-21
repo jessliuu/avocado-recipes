3a3c99e604eafbeaa843352a3ef63f9b7d977955
tszkiu <tszkiu@gmail.com>
2022-04-21 16:14:13 +0200

    Comments

--- main.js
+++ main.js
@@ -22,28 +22,30 @@ mainContainer.classList.add("row");
 // mainContainer.style.justifyContent = "space-evenly";
 body.appendChild(mainContainer);
 
+//
+//
+//START HERE
+
 for (i = 1; i < avocadoData.length; i++) {
-  // console.log(avocadoData[i].id);
+  //SECTION CARD (PARENT)
   let sectionCard = document.createElement("section");
   sectionCard.classList.add("col-md-3");
   sectionCard.classList.add("col-sm-6");
   sectionCard.style.border = "1px solid #d387ab";
   sectionCard.style.borderTop = "0px";
-  // if (i % 2 === 0) {
-  //   sectionCard.style.backgroundColor = "#eec0c6";
-  // }
   sectionCard.style.display = "flex";
   sectionCard.style.flexDirection = "column";
-
   sectionCard.style.alignItems = "center";
   sectionCard.style.justifyContent = "flex-end";
   sectionCard.style.padding = "10px";
 
+  //HEADER (CHILD 1)
   let headerContainer = document.createElement("div");
   let h5 = document.createElement("h5");
   h5.innerHTML = avocadoData[i].title;
   h5.style.color = "#d387ab";
 
+  //IMAGE (CHILD 2)
   let imgContainer = document.createElement("div");
   let img = document.createElement("img");
   img.setAttribute("src", avocadoData[i].image);
@@ -56,11 +58,13 @@ for (i = 1; i < avocadoData.length; i++) {
   sectionCard.appendChild(imgContainer);
   mainContainer.appendChild(sectionCard);
 
+  //INGREDIENTS (CHILD 3)
+
   let ingredientsContainer = document.createElement("div");
-  // ingredientsContainer.style.alignSelf = "flex-start";
   ingredientsContainer.classList.add("align-self-sm-start");
   ingredientsContainer.classList.add("align-self-xxl-center");
 
+  //Tags (CHILD 3.1)
   let tags = document.createElement("p");
   tags.innerHTML = "Tags: ";
 
@@ -76,18 +80,20 @@ for (i = 1; i < avocadoData.length; i++) {
   }
   sectionCard.appendChild(tags);
 
+  //Likes (CHILD 3.2)
   let likes = document.createElement("p");
   likes.innerHTML = avocadoData[i].likes + " Likes";
   ingredientsContainer.appendChild(likes);
 
+  //Other Ingredientss (CHILD 3.3)
   let otheringredients = document.createElement("p");
   otheringredients.innerHTML = "Other ingredients:";
   ingredientsContainer.appendChild(otheringredients);
 
+  //Other Ingredient List (CHILD 3.3.1)
   let ingredientsList = document.createElement("ul");
 
   if (avocadoData[i].missedIngredients.length <= 3) {
-    // console.log("inside  if");
     for (j = 0; j < avocadoData[i].missedIngredients.length; j++) {
       let eachIngredient = document.createElement("li");
       eachIngredient.innerHTML = avocadoData[i].missedIngredients[j].name;
@@ -102,18 +108,13 @@ for (i = 1; i < avocadoData.length; i++) {
     let seeMore = document.createElement("p");
     seeMore.classList.add("leading");
 
-    // let otherIng = [];
-    // otherIng.push();
-
-    // seeMore.type = "button";
-    // seeMore.classList.add("btn btn-outline-secondary");
     seeMore.innerHTML = "And more... 	&#8964;";
     seeMore.style.opacity = "70%";
     ingredientsList.appendChild(seeMore);
 
     for (a = 0; a < 3; a++) {
       console.log(a);
-      // console.log("inside else");
+
       let eachIngredient = document.createElement("li");
       console.log(eachIngredient[j]);
       eachIngredient.innerHTML = avocadoData[i].missedIngredients[j].name;
