ce72368803b1dacab821e930fc16c4ac9dee46ae
tszkiu <tszkiu@gmail.com>
2022-04-30 16:01:37 +0200

    dynamic fetch

--- main.js
+++ main.js
@@ -1,55 +1,110 @@
-const url1 =
-  "https://api.spoonacular.com/recipes/findByIngredients?apiKey=5bb2ba85e71141829b3107f5df442fce&ingredients=avocado&number=100";
-const url2 =
-  "https://api.spoonacular.com/recipes/informationBulk?apiKey=5bb2ba85e71141829b3107f5df442fce&ids=633165%2C%20634048%2C%20648439%2C%201697397%2C%201512847%2C%20715543%2C%20633144%2C%20633160%2C%20650751%2C%20664997%2C%20633157%2C%20649335%2C%20664394%2C%20643455%2C%20633123%2C%20660108%2C%20715521%2C%20780000%2C%20640062%2C%20633132%2C%20645479%2C%20800754%2C%20640959%2C%20650789%2C%20654430%2C%20633120%2C%20633264%2C%20642780%2C%20633141%2C%20650471%2C%20673436%2C%20645988%2C%20663054%2C%201160166%2C%20633126%2C%20632788%2C%20633133%2C%20637999%2C%20643428%2C%20650860%2C%20633117%2C%20633119%2C%20637480%2C%20647608%2C%20650809%2C%20646632%2C%20655785%2C%20665178%2C%20715544%2C%20716437%2C%20633139%2C%20662670%2C%20644593%2C%20658495%2C%20651707%2C%20636676%2C%20643861%2C%20982371%2C%20638593%2C%20645687%2C%20656519%2C%20661431%2C%20635074%2C%20661188%2C%20638741%2C%20640311%2C%20157344%2C%20660024%2C%20661126%2C%20642395%2C%20649343%2C%20657679%2C%201096017%2C%20622825%2C%20660494%2C%20653014%2C%20157399%2C%20795751%2C%20645730%2C%20664501%2C%20633166%2C%20661864%2C%20656723%2C%20653200%2C%20663050%2C%20640990%2C%20637102%2C%20646461%2C%20679509%2C%20469862%2C%20715415%2C%20631912%2C%20651585%2C%20662243%2C%20664595%2C%20650377%2C%20639388%2C%20645608%2C%20664429";
-const options = { method: "GET" };
-
-const fetchDataFunction = () => {
-  fetch(url1)
-    .then((res1) => {
-      console.log("res1 success!", res1);
-      return res1.json();
-    })
-    .then((data1) => {
-      console.log(data1);
-      let fetchAvocadoData = data1;
-      return fetch(url2);
-    })
-    .then((res2) => {
-      console.log("res2 success!", res2);
-      return res2.json();
-    })
-    .then((data2) => {
-      console.log(data2);
-      let fetchRecipeData = data2;
-      // createCards(fetchRecipeData, fetchAvocadoData);
-    })
-    .catch((e) => {
-      console.log("ERROR!", e);
-    });
-};
-
-// fetchDataFunction();
+const apikey = "5bb2ba85e71141829b3107f5df442fce";
 
 let body = document.querySelector("body");
 
 let mainContainer = document.createElement("main");
 mainContainer.classList.add("row");
 body.appendChild(mainContainer);
+// const url1 =
+//   "https://api.spoonacular.com/recipes/findByIngredients?apiKey=5bb2ba85e71141829b3107f5df442fce&ingredients=avocado&number=100";
+// const url2 =
+//   "https://api.spoonacular.com/recipes/informationBulk?apiKey=5bb2ba85e71141829b3107f5df442fce&ids=633165%2C%20634048%2C%20648439%2C%201697397%2C%201512847%2C%20715543%2C%20633144%2C%20633160%2C%20650751%2C%20664997%2C%20633157%2C%20649335%2C%20664394%2C%20643455%2C%20633123%2C%20660108%2C%20715521%2C%20780000%2C%20640062%2C%20633132%2C%20645479%2C%20800754%2C%20640959%2C%20650789%2C%20654430%2C%20633120%2C%20633264%2C%20642780%2C%20633141%2C%20650471%2C%20673436%2C%20645988%2C%20663054%2C%201160166%2C%20633126%2C%20632788%2C%20633133%2C%20637999%2C%20643428%2C%20650860%2C%20633117%2C%20633119%2C%20637480%2C%20647608%2C%20650809%2C%20646632%2C%20655785%2C%20665178%2C%20715544%2C%20716437%2C%20633139%2C%20662670%2C%20644593%2C%20658495%2C%20651707%2C%20636676%2C%20643861%2C%20982371%2C%20638593%2C%20645687%2C%20656519%2C%20661431%2C%20635074%2C%20661188%2C%20638741%2C%20640311%2C%20157344%2C%20660024%2C%20661126%2C%20642395%2C%20649343%2C%20657679%2C%201096017%2C%20622825%2C%20660494%2C%20653014%2C%20157399%2C%20795751%2C%20645730%2C%20664501%2C%20633166%2C%20661864%2C%20656723%2C%20653200%2C%20663050%2C%20640990%2C%20637102%2C%20646461%2C%20679509%2C%20469862%2C%20715415%2C%20631912%2C%20651585%2C%20662243%2C%20664595%2C%20650377%2C%20639388%2C%20645608%2C%20664429";
 
-//
-//
-controller();
+// const fetchDataFunction = (url1, url2) => {
+//   fetch(url1)
+//     .then((res1) => {
+//       console.log("res1 success!", res1);
+//       return res1.json();
+//     })
+//     .then((data1) => {
+//       console.log(data1);
+//       let fetchAvocadoData = data1;
+//       return fetch(url2);
+//     })
+//     .then((res2) => {
+//       console.log("res2 success!", res2);
+//       return res2.json();
+//     })
+//     .then((data2) => {
+//       console.log(data2);
+//       let fetchRecipeData = data2;
+//       // createCards(fetchRecipeData, fetchAvocadoData);
+//     })
+//     .catch((e) => {
+//       console.log("ERROR!", e);
+//     });
+// };
 
-function controller() {
-  createCards(recipeData, avocadoData);
-  // createDropDown();
-  setEventListener(recipeData, avocadoData);
-}
+// fetchDataFunction();
+
+const getDataAsync = async (ingredient) => {
+  const url1 = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${apikey}&ingredients=${ingredient}&number=4`;
+  try {
+    const response = await fetch(url1);
+    const data = await response.json();
+    console.log("data", data);
+
+    let ids = [];
+    for (let i = 0; i < data.length; i++) {
+      ids.push(data[i].id);
+    }
+    console.log(ids);
+
+    let url2 = `https://api.spoonacular.com/recipes/informationBulk?apiKey=${apikey}&ids=`;
+    for (j = 0; j < ids.length; j++) {
+      url2 += ids[j] + "%2C%20";
+    }
+    console.log(url2);
+    const response2 = await fetch(url2);
+    const data2 = await response2.json();
+    console.log("data2", data2);
+    // let allData = [data, data2];
+    // console.log(allData);
+    // return allData;
+
+    createCards(data2, data);
+    setEventListener(data2, data);
+    createEvent();
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+//getDataAsync("avocado");
+
+// const controller = async () => {
+//   const allData = await getDataAsync();
+//   const ingData = allData[0];
+//   const recipeData = allData[1];
+//   console.log(ingData);
+//   createCards(recipeData, ingData);
+//   setEventListener(recipeData, ingData);
+//   createEvent()
+// };
+
+console.log(appleData[2].missedIngredients[1].name);
+createCards(appleRecipes, appleData);
+// setEventListener(appleRecipes, appleData);
+
+const createEvent = () => {
+  let ingredient = "";
+
+  const search = document.getElementById("ing-search");
+
+  search.addEventListener("change", (event) => {
+    ingredient = event.target.value;
+    console.log("ingredient", ingredient);
+  });
+  search.addEventListener("keyup", (event) => {
+    if (event.key === "Enter") {
+      getDataAsync(ingredient);
+    }
+  });
+};
 
 function createCards(recipeData, avocadoData) {
   mainContainer.innerHTML = "";
-  for (i = 1; i < avocadoData.length; i++) {
+  for (i = 0; i < avocadoData.length; i++) {
     for (x = 0; x < recipeData.length; x++) {
       let identifier = avocadoData[i].id;
       if (identifier === recipeData[x].id) {
@@ -272,14 +327,15 @@ function createDropDown() {
 }
 
 function setEventListener(rData, aData) {
-  document.querySelector("#tags").addEventListener("change", (event) => {
+  let dropdown = document.querySelector(".tags");
+  dropdown.addEventListener("change", (event) => {
     filterByDropDown(rData, aData);
   });
 }
 
 function filterByDropDown(rData, aData) {
   let filteredData = [];
-  const dropDownValue = document.querySelector("#tags").value;
+  const dropDownValue = document.querySelector(".tags").value;
   if (dropDownValue === "Vegetarian") {
     filteredData = rData.filter((r) => {
       return r.vegetarian === true;
